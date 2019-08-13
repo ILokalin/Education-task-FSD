a894c8754aef5e2ae61782927c97f6e9bb728c2e
Ilya Lokalin <ilya-1974@yandex.ru>
2019-08-13 15:34:09 +0500

    test with text-field

---
+++ src/pages/index/index.js
@@ -0,0 +1,2 @@
+import './index.scss'
+import '../../modules/text-field/text-field'
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const merge = require('webpack-merge')
+const pug = require('./webpack/pug')
+const scss = require('./webpack/scss')
+const images = require('./webpack/images')
+const fonts = require('./webpack/fonts')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const PATHS = {
+    source: path.join(__dirname, 'src'),
+    build: path.join(__dirname, 'build')
+}
+
+const common = merge([{
+        entry: {
+            'index': PATHS.source + '/pages/index/index.js'
+        },
+        output: {
+            path: PATHS.build,
+            filename: 'js/[name].js'
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                chunks: ['index', 'common'],
+                template: PATHS.source + '/pages/index/index.pug'
+            })
+        ]
+    },
+    pug(),
+    scss(),
+    images(),
+    fonts()
+])
+
+module.exports = function (env) {
+    if (env === 'prodduction') {
+        return common
+    }
+    if (env === 'development') {
+        return common
+    }
+}
