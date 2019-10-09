21e5bfcf0430652e1a964a275f62bd08ef5886e7
ILokalin <51790923+ILokalin@users.noreply.github.com>
2019-10-09 23:54:55 +0500

    мелкие правки - убрает лишние импорты

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -19,29 +19,13 @@ import { Pager } from './../../modules/pagination/pagination';
 
 import { MaskedTextField } from '../../modules/masked-text-field/masked-text-field';
 
+
 var ready = (callback) => {
     if (document.readyState != "loading") callback();
     else document.addEventListener("DOMContentLoaded", callback);
   }
 
 ready(() => { 
-    // const maskedTextDate = new MaskedTextField('date')
-
-    // const demoDropDown = new dropdown('.dropdownVisitors')
-    // const demoDropDown2 = new dropdown('.dropdownEnvarenment')
-    // const expCheckBoxList = expCheckboxList('.checkList-test')
-        
-    // const rangeSlider = new RangeSlider()
-    // // ur.update() //метод для изменений
-    // // let foog = ur.getValue() //чтение состояния
-
-    // const pager = new Pager({
-    //     items: 15,
-    //     itemsOnPage: 5,
-    //     element: '.pager'
-    // })
-
-
 
 });
 
