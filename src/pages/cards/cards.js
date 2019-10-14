6f750f8737481155caa3b95fdfdf1fcbe52c26e0
ILokalin <51790923+ILokalin@users.noreply.github.com>
2019-10-14 22:23:08 +0500

    добавляет карточку номера

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -1,6 +1,11 @@
+// Styles
 import './cards.scss'
 
+// Images
+import '../../room-images/room-840.jpg'
+import '../../room-images/room-888.jpg'
 
+// modules
 import { MaskedTextField } from '../../modules/masked-text-field/masked-text-field';
 
 
