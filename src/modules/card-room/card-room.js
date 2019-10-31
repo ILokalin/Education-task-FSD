73fd6ef03153b1f1f0dffe4fb674c83da5060ee8
ILokalin <51790923+ILokalin@users.noreply.github.com>
2019-11-01 01:52:39 +0500

    create slider with radio-buttons in card-room.js

--- src/modules/card-room/card-room.js
+++ src/modules/card-room/card-room.js
@@ -1,25 +1,75 @@
 export default class {
   constructor() {
-    const con = console.log();
-    // con('work');
-    console.log('eeeee')
-    this.cardsList = []
-    const roomCard = document.querySelectorAll('.room-card');
+    const roomCard = document.querySelectorAll('.card-room');
+
 
     roomCard.forEach((card) => {
       const inputList = card.querySelectorAll('.card-room__input');
       inputList.forEach((inputSelector) => {
-        inputSelector.addEventListener('click', this.changeImage(event));
+        inputSelector.addEventListener('click', (e) => {this.onCangeRadio(e)});
       })
+
+      const sliderControl = card.querySelector('.card-room__controls');
+      sliderControl.addEventListener('click', (e) => {this.onChangeSlider(e)});
     })
 
 
-
-    // this.changeImage = this.changeImage.bind(this);
+    
   }
 
-  changeImage (event) {
-    const imageNum = event.target.value
-    console.log(event.target)
+  checkCurentPicture(parent) {
+    // debugger
+    return parent.querySelector("[type='radio']:checked").value;
   }
+
+  renderPicture(roomObject) {
+    const {parent, index} = roomObject;
+    const fileName = './images/room-' + parent.dataset.room + '-' + index + '.jpg';
+    // console.log(fileName);
+    parent.querySelector('.card-room__image').setAttribute('src', fileName);
+  }
+
+  checkedIndex(roomObject) {
+    const {parent, index} = roomObject;
+    const element = parent.querySelectorAll('.card-room__input')
+    parent.querySelectorAll('.card-room__input')[index-1].setAttribute('checked', 'true')
+    debugger
+  }
+
+  onCangeRadio(e) {
+    const target =e.target;
+    this.renderPicture({
+      parent: target.closest('.card-room'),
+      index: target.value
+    })
+  }
+
+  onChangeSlider(e) {
+    const parent = e.target.closest('.card-room');
+    let currentPicture = Number(this.checkCurentPicture(parent));
+
+
+    
+    if (e.target.classList.contains('card-room__slider-button_prev')) {
+      if (currentPicture === 1) {currentPicture = 5}
+      this.renderPicture({
+        parent: parent,
+        index: --currentPicture
+      })
+      console.log(currentPicture)
+    } else if (e.target.classList.contains('card-room__slider-button_next')) {
+      if (currentPicture === 4) {currentPicture = 0}
+      this.renderPicture({
+        parent: parent,
+        index: ++currentPicture
+      })
+      console.log(currentPicture)
+    }
+
+    this.checkedIndex({
+      parent: parent,
+      index: currentPicture
+    })
+  }
+
 }
--- src/pages/search-room/search-room.js
+++ src/pages/search-room/search-room.js
@@ -1,25 +1,64 @@
 import './search-room.scss';
 import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
 
-import '../../images/room-350.jpg';
-import '../../images/room-352.jpg';
-import '../../images/room-444.jpg';
-import '../../images/room-450.jpg';
-import '../../images/room-666.jpg';
-import '../../images/room-678.jpg';
-import '../../images/room-740.jpg';
-import '../../images/room-840.jpg';
-import '../../images/room-856.jpg';
-import '../../images/room-888.jpg';
-import '../../images/room-980.jpg';
-import '../../images/room-982.jpg';
+import '../../images/room-350-1.jpg';
+import '../../images/room-352-1.jpg';
+import '../../images/room-444-1.jpg';
+import '../../images/room-450-1.jpg';
+import '../../images/room-666-1.jpg';
+import '../../images/room-678-1.jpg';
+import '../../images/room-740-1.jpg';
+import '../../images/room-840-1.jpg';
+import '../../images/room-856-1.jpg';
+import '../../images/room-888-1.jpg';
+import '../../images/room-980-1.jpg';
+import '../../images/room-982-1.jpg';
+
+import '../../images/room-350-2.jpg';
+import '../../images/room-352-2.jpg';
+import '../../images/room-444-2.jpg';
+import '../../images/room-450-2.jpg';
+import '../../images/room-666-2.jpg';
+import '../../images/room-678-2.jpg';
+import '../../images/room-740-2.jpg';
+import '../../images/room-840-2.jpg';
+import '../../images/room-856-2.jpg';
+import '../../images/room-888-2.jpg';
+import '../../images/room-980-2.jpg';
+import '../../images/room-982-2.jpg';
+
+import '../../images/room-350-3.jpg';
+import '../../images/room-352-3.jpg';
+import '../../images/room-444-3.jpg';
+import '../../images/room-450-3.jpg';
+import '../../images/room-666-3.jpg';
+import '../../images/room-678-3.jpg';
+import '../../images/room-740-3.jpg';
+import '../../images/room-840-3.jpg';
+import '../../images/room-856-3.jpg';
+import '../../images/room-888-3.jpg';
+import '../../images/room-980-3.jpg';
+import '../../images/room-982-3.jpg';
+
+import '../../images/room-350-4.jpg';
+import '../../images/room-352-4.jpg';
+import '../../images/room-444-4.jpg';
+import '../../images/room-450-4.jpg';
+import '../../images/room-666-4.jpg';
+import '../../images/room-678-4.jpg';
+import '../../images/room-740-4.jpg';
+import '../../images/room-840-4.jpg';
+import '../../images/room-856-4.jpg';
+import '../../images/room-888-4.jpg';
+import '../../images/room-980-4.jpg';
+import '../../images/room-982-4.jpg';
 
 // self modules import
 import {Pager} from '../../modules/pagination/pagination';
 import {RangeSlider} from '../../modules/range-slider/range-slider';
 import {dropdown} from '../../modules/dropdown/dropdown';
 import {expCheckboxList} from '../../modules/expandable-checkbox-list/expandable-checkbox-list';
-// import CardSelector from '../../modules/card-room/card-room';
+import CardSelector from '../../modules/card-room/card-room';
 // const cardSelector = new CardSelector();
 
 var ready = (callback) => {
@@ -28,6 +67,7 @@ var ready = (callback) => {
 }
 
 ready(() => { 
+  const cardSelector = new CardSelector();
   // const maskedTextDate = new MaskedTextField('date')
 
   const demoDropDownVisitors = new dropdown('.dropdown-visitors')
